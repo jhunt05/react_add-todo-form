aa81a22e63bf05481c44d4cce3912545bc82d461
Viktoriia <jhunt@ukr.net>
2019-11-04 14:38:22 +0200

    Updates

---
+++ precache-manifest.faf8cc37fa279c31cfba74316ef0d546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8165a14a95b255898d92a189cb57713a",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "923adf210a8bfe85f531",
+    "url": "/react_add-todo-form/static/css/main.7a541dc9.chunk.css"
+  },
+  {
+    "revision": "1b2cfe77f2bb9f146d39",
+    "url": "/react_add-todo-form/static/js/2.3c77df80.chunk.js"
+  },
+  {
+    "revision": "923adf210a8bfe85f531",
+    "url": "/react_add-todo-form/static/js/main.dbfbd9fa.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
